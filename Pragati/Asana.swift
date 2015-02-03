63ac4d32f6e2dc31fee6ef233a8bd75ee90283b2
Jessica Le <jessicale90@gmail.com>
2015-02-02 21:48:52 -0600

    added sample data and Asana object

---
+++ Pragati/Asana.swift
@@ -0,0 +1,21 @@
+//
+//  Asana.swift
+//  Pragati
+//
+//  Created by Jessica Le on 2/2/15.
+//  Copyright (c) 2015 LoveLeJess. All rights reserved.
+//
+
+import UIKit
+
+class Asana: NSObject {
+    var sanskrit: String
+    var english: String
+    
+    init(sanskrit: String, english:String){
+        self.sanskrit = sanskrit
+        self.english = english
+        super.init()
+        
+    }
+}
---
+++ Pragati/SampleData.swift
@@ -0,0 +1,14 @@
+//
+//  SampleData.swift
+//  Pragati
+//
+//  Created by Jessica Le on 2/2/15.
+//  Copyright (c) 2015 LoveLeJess. All rights reserved.
+//
+
+import Foundation
+
+let asanaData = [Asana(sanskrit: "Adho Mukha Svanasana", english: "Downward Facing Dog"),
+                Asana(sanskrit: "Urdhva Dhanursana", english: "Upward Bow Pose")
+                Asana(sanskrit: "Trikonasana (Left)", english: "Triangle (Left)"),
+                Asana(sanskrit: "Trikonasana (Right)", english: "Triangle (Right)")]
