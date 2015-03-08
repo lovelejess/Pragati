e8c48d551c0b6b3c4043913708c6159c4c1d7afd
Jessica Le <jessicale90@gmail.com>
2015-03-08 12:12:19 -0500

    fixed cell spacing

--- Pragati/UserPhotoCollectionViewController.swift
+++ Pragati/UserPhotoCollectionViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UserPhotoCollectionViewController: UICollectionViewController {
+class UserPhotoCollectionViewController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,28 +25,16 @@ class UserPhotoCollectionViewController: UICollectionViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        //#warning Incomplete method implementation -- Return the number of sections
         return 1
     }
 
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //#warning Incomplete method implementation -- Return the number of items in the section
-        return 30
+        return 20
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
