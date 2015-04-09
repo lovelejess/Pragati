1520811ae8588339bac516ec72d44b359c173082
Jessica Le <jessicale90@gmail.com>
2015-04-08 19:42:19 -0500

    add tests

---
+++ PragatiTests/AsanaTest.swift
@@ -0,0 +1,24 @@
+//
+//  AsanaTableViewControllerTest.swift
+//  Pragati
+//
+//  Created by Jessica Le on 4/2/15.
+//  Copyright (c) 2015 LoveLeJess. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AsanaTest: XCTestCase {
+    
+    func testAsanaIsInitializedWithValues(){
+        let asana = Asana(sanskrit: "Adho Mukha Svanasana", english: "Downward Facing Dog", defaultImage: UIImage(named:"downwardFacingDog.png"))
+        let sanskrit = "Adho Mukha Svanasana"
+        let english = "Downward Facing Dog"
+        
+        XCTAssertEqual(sanskrit, asana.sanskrit, "Sanskrit is not set properly")
+        XCTAssertEqual(english, asana.english, "English is not set properly")
+        XCTAssertNotNil(asana.defaultImage?, "Default Image is not set properly")
+    }
+}
+
